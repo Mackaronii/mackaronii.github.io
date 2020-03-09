290b0a6dee80fd34b376a4ee44bf73912c209c25
Jonathan Mack <johnny.mack1998@gmail.com>
2020-03-08 23:57:00 -0400

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# My Personal Website
-
-Hosted on GitHub Pages at https://mackaronii.github.io/
---
+++ precache-manifest.ab703b4d6d3fe9bdd6fe4f1f8c00f524.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a86a6d1de691d59f0ac7df4b26ff8c3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5ba0671d6568c0018e8",
+    "url": "/static/css/2.e672b052.chunk.css"
+  },
+  {
+    "revision": "564e90cc57dd44a67604",
+    "url": "/static/css/main.b9e5d2a0.chunk.css"
+  },
+  {
+    "revision": "c5ba0671d6568c0018e8",
+    "url": "/static/js/2.bf4131bd.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.bf4131bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "564e90cc57dd44a67604",
+    "url": "/static/js/main.04c7df81.chunk.js"
+  },
+  {
+    "revision": "96bccdf7f7e5584f9a2f",
+    "url": "/static/js/runtime-main.35061494.js"
+  }
+]);
